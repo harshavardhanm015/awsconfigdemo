c5b57fa20776bcb24cedb88fef6964ee9757d789
Harshavardhan Reddy Malipatlolla <harshasaim09@gmail.com>
2018-06-18 22:23:26 -0500

    FirstPush

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog{
+private String name;
+private int age;
+Dog(String name, int age){
+this.name=name;
+this.age=age;
+}
+void display(){System.out.println("Dog Name "+ name +"And Dog Age "+ age);
+	}
+public static void main(String[] args){
+Dog d1= new Dog("Puppy", 1);
+d1.display();
+}
+}
